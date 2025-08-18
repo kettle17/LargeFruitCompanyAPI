28b1b98d3069a962dc8674bd3b2013a1466fda96
kettle17 <trainee.callum.peters@sigmalabs.co.uk>
2025-08-18 13:50:52 +0100

    first commit

---
+++ src/main/java/org/example/largefruitcompanyapi/LargeFruitCompanyApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.largefruitcompanyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LargeFruitCompanyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LargeFruitCompanyApiApplication.class, args);
+    }
+
+}
